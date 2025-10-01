ee205f14c74c3ebbd62f58ea11a0fdabb43af6e5
SoulVage <amirazarbahram1234@gmail.com>
2025-10-01 22:58:01 +0330

    `Refactor dropdown menu in products/index.html and update corresponding CSS and JavaScript files`

--- src/products/script/dropdown.js
+++ src/products/script/dropdown.js
@@ -2,8 +2,23 @@ const dropdown = document.querySelectorAll('.dropdown');
 
 dropdown.forEach((drop) => {
     const content = drop.nextElementSibling;
+    const searchInput = content.querySelector(".search-input")
+
     drop.addEventListener('click', () => {
         content.classList.toggle('active');
-        drop.querySelector("svg").classList.toggle('rotate-180');
+        drop.querySelector("svg").classList.toggle('rotate-180');      
     })
+    if (!searchInput) return;
+    searchInput.addEventListener("keyup", () => {
+        const filter = searchInput.value.trim().toLowerCase();
+        const items = content.querySelectorAll(".dropdown-items");
+        items.forEach(item => {
+            let value = item.querySelector("label").innerHTML.toLowerCase();
+            if (!filter || value.includes(filter)) {
+                item.classList.remove("opacity-30");
+            } else {
+                item.classList.add("opacity-30");
+            }            
+        })
+    })  
 })
